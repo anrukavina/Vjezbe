c30233e25bdabaa2102f3d3ca90ba26bd1833157
anrukavina <antunrukavina@hotmail.com>
2022-05-07 13:52:14 +0200

    Kreiranje nove baze

---
+++ SQL/zastita_zivotinja.sql
@@ -0,0 +1,29 @@
+# baza podataka za udrugu za zastitu zivotinja
+# naredba za izvođenje
+# 
+
+drop database if exists zastita_zivotinja;
+create database zastita_zivotinja;
+
+create table djelatnik (
+    ime     varchar(50),
+    prezime varchar(50),
+    placa   varchar(50),
+    oib     char(11)
+);
+
+create table sticenik (
+    vrsta   varchar(50),
+    pasmina varchar(50),
+    boja    varchar(50),
+    dob     varchar(50),
+    ime     varchar(50)
+);
+
+create table prostor (
+    tip     varchar(50),
+    duljina varchar(50),
+    sirina  varchar(50),
+    visina  varchar(50)
+);
+
