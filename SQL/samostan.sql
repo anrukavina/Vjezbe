2896500a22bc77002bbca71d23023f573627ae1d
anrukavina <antunrukavina@hotmail.com>
2022-05-12 17:02:11 +0200

    Definiranje primarnih ključeva i tipova podataka

--- SQL/samostan.sql
+++ SQL/samostan.sql
@@ -1,30 +1,40 @@
 # baza podataka za samostan
 # naredba za izvođenje
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\samostan.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\samostan.sql
 
 drop database if exists samostan;
-create database samostan;
+create database samostan default charset utf8mb4;
 use samostan;
 
 create table osoba (
-    ime     varchar(50),
-    prezime varchar(50),
+    sifra   int not null primary key auto_increment,
+    ime     varchar(50) not null,
+    prezime varchar(50) not null,
     oib     char(11)
 );
 
 create table svecenik (
-    osoba varchar(50),
-    posao varchar(50)
+    sifra              int not null primary key auto_increment,
+    osoba              int not null,
+    posao              int not null,
+    nadredeni_svecenik int not null
 );
 
 create table posao (
-    naziv    varchar(50),
-    vrsta    varchar(50),
-    trajanje varchar(50)
+    sifra             int not null primary key auto_increment,
+    naziv             varchar(50),
+    vrsta             varchar(50),
+    vrijeme_pocetka   datetime,
+    vrijeme_zavrsetka datetime,
+    svecenik          int not null,
+    nadredeni_svecenik int not null
 );
 
 create table nadredeni_svecenik (
-    osoba  varchar(50),
-    posao  varchar(50),
-    nadzor varchar(50)
+    sifra              int not null primary key auto_increment,
+    osoba              int not null,
+    posao              int not null,
+    podredeni_svecenik int not null
 );
+
+# definiranje vanjskih ključeva
