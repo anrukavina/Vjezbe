342dce5c763e883743d5b77fbddd26e33dd5ff11
anrukavina <antunrukavina@hotmail.com>
2022-05-12 18:21:53 +0200

    Definiranje primarnih ključeva i tipova podataka

--- SQL/urar_silvija.sql
+++ SQL/urar_silvija.sql
@@ -1,30 +1,40 @@
 # baza podataka za urar silvija
 # naredba za izvođenje
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\urar_silvija.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8< C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\urar_silvija.sql
 
 drop database if exists urar_silvija;
-create database urar_silvija;
+create database urar_silvija default charset utf8mb4;
 use urar_silvija;
 
 create table osoba (
-    ime varchar(50),
-    prezime varchar(50),
-    oib char(11)
+    sifra   int not null primary key auto_increment;
+    ime     varchar(50) not null,
+    prezime varchar(50) not null,
+    oib     char(11)
+);
+
+create table urar (
+    sifra int not null primary key auto_increment,
+    osoba int not null
 );
 
 create table korisnik (
-    osoba varchar(50),
-    email varchar(50)
-);
-
-create table sat (
-    vrsta varchar(50),
-    proizvodac varchar(50),
-    model varchar(50),
-    boja varchar(50)
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    sat   int not null
 );
 
 create table segrt (
-    osoba varchar(50),
-    placa varchar(50)
+    sifra int not null primary key auto_increment,
+    placa decimal(18,2),
+    osoba int not null
 );
+
+create table sat (
+    sifra      int not null primary key auto_increment,
+    vrsta      varchar(50),
+    proizvodac varchar(50),
+    model      varchar(50),
+    boja       varchar(50)
+);
+
