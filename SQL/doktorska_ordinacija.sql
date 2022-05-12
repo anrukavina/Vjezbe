4546a0e9541bd4d73ea669b17dc519972ca00eb4
anrukavina <antunrukavina@hotmail.com>
2022-05-12 18:52:32 +0200

    Definiranje primarnih ključeva i tipova podataka

--- SQL/doktorska_ordinacija.sql
+++ SQL/doktorska_ordinacija.sql
@@ -1,31 +1,40 @@
 # baza podataka za doktorsku ordinaciju
 # naredba za izvođenje
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\doktorska_ordinacija.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\doktorska_ordinacija.sql
 
 drop database if exists doktorska_ordinacija;
 create database doktorska_ordinacija;
 use doktorska_ordinacija;
 
 create table osoba (
-    ime     varchar(50),
-    prezime varchar(50),
+    sifra   int not null primary key auto_increment,
+    ime     varchar(50) not null,
+    prezime varchar(50) not null,
     oib     char(11)
 );
 
 create table doktor (
-    osoba           varchar(50),
-    br_telefona     varchar(50),
+    sifra           int not null primary key auto_increment,
+    osoba           int not null,
     specijalizacija varchar(50)
 );
 
 create table pacijent (
-    osoba     varchar(50),
-    email     varchar(50),
-    dijagnoza varchar(50)
+    sifra     int not null primary key auto_increment,
+    osoba     int not null,
+    email     varchar(100)
 );
 
 create table medicinska_sestra (
-    osoba          varchar(50),
+    sifra          int not null primary key auto_increment,
+    osoba          int not null,
     strucna_sprema varchar(50),
-    placa          varchar(50)
+    placa          decimal(18,2)
+);
+
+create table lijecenje (
+    sifra    int not null primary key auto_increment,
+    pocetak  datetime,
+    kraj     datetime,
+    izljecen boolean    
 );
