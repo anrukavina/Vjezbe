8d6e80a5add2f5a5be51d1261e737975cd3650f9
anrukavina <antunrukavina@hotmail.com>
2022-05-23 13:28:58 +0200

    Kreiranje nove baze

---
+++ SQL/opg/opg.sql
@@ -0,0 +1,34 @@
+# baza podataka za obiteljsko poljoprivredno gospodarstvo
+# naredba za izvođenje
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\opg\opg.sql
+
+drop database if exists opg;
+create database opg default charset utf8mb4;
+use opg;
+
+create table proizvod (
+    sifra     int not null primary key auto_increment,
+    naziv     varchar(50),
+    vrsta     varchar(50),
+    cijena    decimal(18,2),
+    djelatnik int not null
+);
+
+create table sirovina (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50)
+);
+
+create table djelatnik (
+    sifra   int not null primary key auto_increment,
+    ime     varchar(50),
+    prezime varchar(50),
+    oib     char(11),
+    placa   decimal(18,2)
+);
+
+create table proizvodnja (
+    sifra    int not null primary key auto_increment,
+    proizvod int not null,
+    sirovina int not null
+);
