b917768115963d2c30839fe1f91409667ab698b6
anrukavina <antunrukavina@hotmail.com>
2022-05-25 18:44:12 +0200

    Kreiranje nove baze podataka

---
+++ SQL/kud/kud.sql
@@ -0,0 +1,34 @@
+# baza podataka za kulturno umjetničko društvo
+# naredba za izvođenje:
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\antun\OneDrive\Dokumenti\GitHub\Vjezbe\SQL\kud\kud.sql
+
+drop database if exists kud;
+create database kud default charset utf8mb4;
+use kud;
+
+create table clan (
+    sifra   int not null primary key auto_increment
+    ime     varchar(50) not null,
+    prezime varchar(50) not null,
+    starost int    
+);
+
+create table mjesto (
+    sifra int not null primary key auto_increment,
+    naziv          varchar(50) not null,
+    postanski_broj varchar(10) not null,
+    drzava         varchar(50) not null
+);
+
+create table nastup (
+    sifra       int not null primary key auto_increment,
+    naziv       varchar(50),
+    broj_kudova int,
+    mjesto int not null
+);
+
+create table nastupanje (
+    sifra  int not null primary key auto_increment,
+    nastup int not null,
+    clan   int not null
+);
