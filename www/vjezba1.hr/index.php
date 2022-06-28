1c1a353ded7d20aff4eefb078f8b4bfc3951d797
anrukavina <antunrukavina@hotmail.com>
2022-06-28 15:22:49 +0200

    Vježbanje prijeloma stranice

---
+++ www/vjezba1.hr/index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+
+<head>
+
+  <!-- Header start -->
+
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vježba 1</title>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/app.css">
+
+  <!-- Header end -->
+
+</head>
+
+<body>
+
+  <div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
+    <button class="menu-icon" type="button" data-toggle></button>
+    <div class="title-bar-title">Menu</div>
+  </div>
+
+  <!-- Navigation bar start -->
+
+    <div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li class="menu-text">VJEŽBA 1</li>
+          <li>
+            <a href="#">One</a>
+            <ul class="menu vertical">
+              <li><a href="#">One</a></li>
+              <li><a href="#">Two</a></li>
+              <li><a href="#">Three</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+        </ul>
+      </div>
+    </div>
+
+  <!-- Navigation bar end -->
+
+  <!-- Body start -->
+
+    <div class="grid-x grid-margin-x" id="body">
+      <div class="cell medium-8 large-3">
+        <div class="primary callout">Cell 1</div>
+      </div>
+      <div class="cell medium-8 large-3">
+        <div class="success callout">Cell 2</div>
+      </div>
+      <div class="cell medium-8 large-3">
+        <div class="warning callout">Cell 3</div>
+      </div>
+      <div class="cell medium-8 large-3">
+        <div class="alert callout">Cell 4 </div>
+      </div>
+    </div>
+
+  <!-- Body end -->
+
+  <!-- Footer start -->
+
+    <div class="grid-x">
+      <div class="sucess callout">
+        &copy; Antun 2022
+      </div>
+    </div>
+
+  <!-- Footer end -->
+
+
+
+  <!-- js_scripts start -->
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+
+  <!-- js_scripts end -->
+
+</body>
+
+</html>
